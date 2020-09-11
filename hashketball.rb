dba3210909fc8678d5df073ff174033c72783de4
Reid Watson <j.watsonreid@gmail.com>
2020-09-11 05:01:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,10 +130,12 @@ end
 
 
 # Write code here
-def num_points_scored(player_search)
-  game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
-      if player[:player_name] == player_search
+
+def num_points_scored(player_name)
+  
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |player|
+      if player[:player_name] == player_name
         return player[:points]
       end
     end
